d7ddcddeeed144b9a2b28985e02c97bbd11d66a5
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-08-17 11:35:23 +0200

    Commit 1 Constrains gesetzt 17.08.2022

--- farborakel.xcodeproj/ViewController.swift
+++ farborakel.xcodeproj/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
+    @IBOutlet var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 120
+    }
+    func randomcolor() -> UIColor{
+        let red: CGFloat = CGFloat(drand48())
+        let green: CGFloat = CGFloat(drand48())
+        let blue: CGFloat = CGFloat(drand48())
+        return UIColor(red:red, green:green, blue:blue, alpha: 1.0)
     }
 
 
+    @IBAction func actionButton(_ sender: UIButton) {
+        backgroundView.backgroundColor = randomcolor()
+        
+    }
+    
+    @IBAction func colorKreis(_ sender: UIButton) {
+        colorView.backgroundColor = randomcolor()
+    }
 }
 
